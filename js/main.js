59250f3bbeed290cdeff88f056d20690d7ff293a
Rahat Ahmed <71262853+protons0010@users.noreply.github.com>
2021-12-21 11:57:48 +0600

    commence

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+
+ 
+	var mixer = mixitup('.container_Box');
+              });
+
+
+// function openNav() {
+// 	document.getElementById("myNav").style.width = "100%"};
+
+
+
+// function closeNav() {
+// 	document.getElementById("myNav").style.width = "0%"};
